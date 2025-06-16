b811ceee349a2fe8d879fa6377067b794d9be752
sennemmi <987780956@qq.com>
2025-06-16 15:57:23 +0800

    数据库连接

---
+++ demo/src/test/java/com/hhu/javawebcrawler/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhu.javawebcrawler.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
